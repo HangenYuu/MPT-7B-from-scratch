1b3acc3ace0f5fff64f8524822c93494d527bd3c
HangenYuu <pnhung1703@gmail.com>
2023-07-11 17:38:24 +0800

    Initial commit

---
+++ setup-lite.sh
@@ -0,0 +1,5 @@
+conda create -y -n nlpbook python=3.10 "pytorch>=2.0.1" pytorch-cuda=11.8 "transformers>=4.28.1" "datasets>=2.11.0" "tokenizers>=0.13.3" sentencepiece optuna einops matplotlib ipywidgets jupyterlab umap-learn seqeval nltk sacrebleu py7zr nlpaug psutil accelerate -c pytorch -c nvidia/label/cuda-11.8.0 -c huggingface -c conda-forge
+
+conda activate nlpbook
+
+pip install fastxtend bertviz
